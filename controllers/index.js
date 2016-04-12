75f03ae55ad0e71c939958131359c26119adfb59
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-04-12 09:56:47 -0700

    changed uppercase

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
-module.exports.api = require('./apiController');
+module.exports.api = require('./apisController');
 module.exports.bands = require('./bandsController');
 module.exports.concerts = require('./concertsController');
 module.exports.users = require('./usersController');
